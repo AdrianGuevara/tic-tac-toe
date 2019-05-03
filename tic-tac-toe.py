9b8acf8ac02575034f9e2b90c6c1fd4d976bf34b
Adrián Donaldo Guevara Salomón <adrian.guevara.ds@gmail.com>
2019-05-03 06:22:17 -0500

    Lack opposite corner case

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,68 +1,122 @@
-emptyMark = '_'
+EMPTY_MARK = '_'
+O_MARK = 'o'
+X_MARK = 'x'
+CENTER = (1,1)
+CORNERS = ((0,0),(0,2),(2,2),(2,0))
 
-def bestPlay(playerMark, grid):
-	return max(possiblePlays(playerMark, grid), key=lambda play: play[1])[0]
+def best_play(board_state, mark_in_play):
+	return max(possible_plays(board_state, mark_in_play), key=lambda play: play[1])
 
-def possiblePlays(playerMark, grid):
-	return [
-	(markPlay(playerMark, emptyPosition, grid),
-	playScore(playerMark, emptyPosition, grid))
-	for emptyPosition in emptyPositions(grid)]
+def possible_plays(board_state, mark_in_play):
+	return [(possible_next_board_state,
+             play_score(possible_next_board_state,
+                        played_position))
+            for possible_next_board_state, played_position
+            in possible_following_board_states(board_state,
+                                               mark_in_play)]
 
-def markPlay(playerMark, emptyPosition, grid):
-	return {
-	position: playerMark if position == emptyPosition else mark
-	for position, mark in grid.items()}
+def possible_following_board_states(board_state, mark_in_play):
+    return [(board_state_after_marking(board_state,
+                                       empty_position,
+                                       mark_in_play),
+             empty_position)
+            for empty_position in empty_positions(board_state)]
 
-def playScore(playerMark, playPosition, grid):
-	return 100 if anyThreeInLine(playerMark, markPlay(playerMark, playPosition, grid)) else 0
+def board_state_after_marking(board_state, position_to_mark, mark):
+	return [[board_state[row][col] if (row, col) != position_to_mark else mark
+             for col in range(len(board_state[row]))]
+            for row in range(len(board_state))]
 
-def anyThreeInLine(playerMark, grid):
-	return any([threeInLine(playerMark, line)
-	for line in gridLines(grid)])
+def play_score(board_state, played_position):
+    line_patterns = adjacent_marks(board_state, played_position)
+    mark_to_check = position_mark(board_state, played_position)
+    return 1000*winning_play(line_patterns, mark_to_check) \
+    + 400*blocking_play(line_patterns, mark_to_check) \
+    + 25*amount_of_two_in_line(line_patterns, mark_to_check) \
+    + 20*amount_of_two_in_line_blocked(line_patterns, mark_to_check) \
+    + 5*(played_position==CENTER) \
+    + (played_position in CORNERS)
 
-def threeInLine(playerMark, line):
-	return all([mark==playerMark for mark in line.values()])
+def adjacent_marks(board_state, interceptor_position):
+    return [[mark for position, mark in line
+             if not position == interceptor_position]
+            for line in board_lines(board_state)
+            if interceptor_position
+            in [position for position, __ in line]]
 
-def positionLines(position, grid):
-	return [line for line in gridLines(grid) if position in line]
+def board_lines(board_state):
+    return [*board_rows(board_state),
+    *board_columns(board_state),
+    *board_diagonals(board_state)]
 
-def gridLines(grid):
-	return gridRows(grid) + gridColumns(grid) + gridDiagonals(grid)
+def board_rows(board_state):
+    return [[((col, row), board_state[col][row])
+          for row in range(len(board_state))]
+         for col in range(len(board_state))]
 
-def gridRows(grid):
-	return [{(i, j): grid.get((i, j)) for j in range(3)} for i in range(3)]
+def board_columns(board_state):
+	return [[((row, col), board_state[row][col])
+          for row in range(len(board_state))]
+         for col in range(len(board_state))]
 
-def gridColumns(grid):
-	return [{(i, j): grid.get((i, j)) for i in range(3)} for j in range(3)]
+def board_diagonals(board_state):
+	return [first_diagonal(board_state), second_diagonal(board_state)]
 
-def gridDiagonals(grid):
-	return [firstDiagonal(grid), secondDiagonal(grid)]
+def first_diagonal(board_state):
+	return [((i, i), board_state[i][i]) for i in range(len(board_state))]
 
-def firstDiagonal(grid):
-	return {(i, i): grid.get((i, i)) for i in range(3)}
+def second_diagonal(board_state):
+	return [((i, j), board_state[i][j]) for i, j
+            in zip(range(len(board_state)), reversed(range(len(board_state))))]
 
-def secondDiagonal(grid):
-	return {(i, j): grid.get((i, j)) for i, j in zip(range(3), reversed(range(3)))}
+def position_mark(board_state, position):
+    return board_state[position[0]][position[1]]
 
-def emptyPositions(grid):
-	return [position for position, mark in grid.items() if mark == emptyMark]
+def winning_play(line_patterns, mark_to_check):
+    return any([all([mark_to_check == mark for mark in line_pattern])
+                for line_pattern in line_patterns])
+
+def blocking_play(line_patterns, mark_to_check):
+    return winning_play(line_patterns, opposite_mark(mark_to_check))
+
+def opposite_mark(mark):
+    return O_MARK if mark == X_MARK else X_MARK
+
+def amount_of_two_in_line(line_patterns, mark_to_check):
+    return sum([two_in_line(line_pattern, mark_to_check)
+                for line_pattern in line_patterns])
+
+def two_in_line(line_pattern, mark_to_check):
+    return mark_to_check in line_pattern and None in line_pattern
+
+def amount_of_two_in_line_blocked(line_patterns, mark_to_check):
+    return amount_of_two_in_line(line_patterns, opposite_mark(mark_to_check))
+
+def empty_positions(board_state):
+	return [(row, col) for row in range(len(board_state))
+         for col in range(len(board_state[row]))
+                          if not board_state[row][col]]
 
 
 
 
-def printGrid(grid):
-	aux = ""
-	for i in range(3):
-		aux = "|"
-		for j in range(3):
-			aux+=str(grid.get((i,j)))+"|"
-		print(aux)
+def play_to_string(play):
+    board_state, score = play
+    aux = board_state_to_string(board_state)
+    aux += '=======\nscore: '+str(score)
+    return aux
 
-initialGrid = {
-(0,0): '_', (0,1): '_', (0,2): '_',
-(1,0): 'o', (1,1): 'o', (1,2): 'x',
-(2,0): 'x', (2,1): 'o', (2,2): 'x'}
-for line in positionLines((1,1), initialGrid):
-	print(line)
-	print('=======')
+def board_state_to_string(board_state):
+    aux = ""
+    for row in range(len(board_state)):
+        aux += "|"
+        for col in range(len(board_state[row])):
+            aux += (board_state[row][col] or EMPTY_MARK) + "|"
+        aux += '\n'
+    return aux;
+
+initial_board_state = ((None, None, None,),
+                       (None, 'O', None,),
+                       (None, None, None))
+print(board_state_to_string(initial_board_state))
+print(play_to_string(best_play(initial_board_state, 'x')))
